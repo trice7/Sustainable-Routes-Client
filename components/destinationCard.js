bad9aadd9bd24815c0b109a48d7628f4bad3e92a
Thomas Rice <ricethomasj@gmail.com>
2024-02-05 20:11:54 -0600

    Fixed data error on getting destinations

--- components/destinationCard.js
+++ components/destinationCard.js
@@ -5,13 +5,14 @@ import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 
 function destinationCard({ location }) {
+  console.warn(location);
   return (
     <Card>
-      {location && <Card.Img variant="top" src={location.location.image} alt={location.location.name} />}
+      {location && <Card.Img variant="top" src={location.image} alt={location.name} />}
       <Card.Body>
-        <Card.Title>{location && location.location.name}</Card.Title>
-        <p className="card-text bold">Description: {location && location.location.description}</p>
-        <Link href={`/details/${location.location.id}`} passHref>
+        <Card.Title>{location && location.name}</Card.Title>
+        <p className="card-text bold">Description: {location && location.description}</p>
+        <Link href={`/details/${location.id}`} passHref>
           <Button variant="primary" className="m-2">VIEW</Button>
         </Link>
       </Card.Body>
